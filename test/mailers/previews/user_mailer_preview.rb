262edd131a8b97adaf714844d9ae6b8b72331d3e
Reo05-code <t.asaishi05@gmail.com>
2025-10-13 14:19:49 +0900

    Add account activation

---
+++ test/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,15 @@
+class UserMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_activation
+  def account_activation
+    user = User.first || User.new(name: "Example User", email: "ataiki655635@gmail.com")
+    user.activation_token = User.new_token
+    UserMailer.account_activation(user)
+  end
+
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/password_reset
+  def password_reset
+    UserMailer.password_reset
+  end
+
+end
